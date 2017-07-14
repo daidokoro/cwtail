597625ea782582f36e11b11a2e6788753ba9f6a4
daidokoro <shaun@daidokoro.io>
2017-07-14 17:40:04 +0100

    removed newlines from log print

--- logger.go
+++ logger.go
@@ -12,22 +12,22 @@ type logger struct {
 
 // Info - Prints info level log statments
 func (l *logger) Info(msg string) {
-	fmt.Printf("%s: %s\n", l.ColorString("info", "green"), msg)
+	fmt.Printf("%s: %s", l.ColorString("info", "green"), msg)
 }
 
 // Warn - Prints warn level log statments
 func (l *logger) Warn(msg string) {
-	fmt.Printf("%s: %s\n", l.ColorString("warn", "yellow"), msg)
+	fmt.Printf("%s: %s", l.ColorString("warn", "yellow"), msg)
 }
 
 // Error - Prints error level log statements
 func (l *logger) Error(msg string) {
-	fmt.Printf("%s: %s\n", l.ColorString("error", "red"), msg)
+	fmt.Printf("%s: %s", l.ColorString("error", "red"), msg)
 }
 
 // Debug - Prints debug level log statements
 func (l *logger) Debug(msg string) {
 	if *l.DebugMode {
-		fmt.Printf("%s: %s\n", l.ColorString("debug", "magenta"), msg)
+		fmt.Printf("%s: %s", l.ColorString("debug", "magenta"), msg)
 	}
 }
